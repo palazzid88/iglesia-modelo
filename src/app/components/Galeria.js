208f5c0cba1a707b5167c11e81d3c2be9f06729f
palazzid88 <palazzid88@gmail.com>
2025-09-08 17:54:20 -0300

    se actualiza componente galeria

--- src/app/components/Galeria.js
+++ src/app/components/Galeria.js
@@ -2,7 +2,6 @@ import { client, urlFor } from "@/lib/sanity";
 
 async function getGaleria() {
   const query = `*[_type == "galeria"]{
-    titulo,
     imagenes
   }`;
   return await client.fetch(query);
@@ -11,19 +10,17 @@ async function getGaleria() {
 export default async function Galeria() {
   const data = await getGaleria();
 
-  console.log("data", data);
-  
+  // Aplanar todas las imágenes de todos los documentos
+  const imagenes = data.flatMap(doc => doc.imagenes || []);
 
-  if (!data) {
+  if (imagenes.length === 0) {
     return <p className="text-center text-gray-500">No hay imágenes cargadas.</p>;
   }
 
   return (
     <section className="py-16 px-4 bg-white text-center">
-      {/* <h2 className="text-3xl font-bold mb-8">{data.titulo || "Galería de Fotos"}</h2> */}
-
       <div className="flex overflow-x-auto gap-4 px-2 pb-4 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
-        {data.imagenes?.map((img, index) => (
+        {imagenes.map((img, index) => (
           <img
             key={index}
             src={urlFor(img).width(400).height(300).url()}
